8b586bed7445079a815d25b54f0e15c5419f05c5
Soha Parvez <31773939+parvez98@users.noreply.github.com>
2020-12-01 17:36:12 -0500

    Normalized data by population and added new dataset

--- covid_correlation.py
+++ covid_correlation.py
@@ -13,14 +13,20 @@ col_list = ["date", "state", "deathIncrease", "totalTestResultsIncrease"]
 df = pd.read_csv('all-states-history.csv', usecols=col_list, sep=",")
 del_states = ["AK", "HI", "PR"]
 state_coord = pd.read_csv('states.csv')
+population_df = pd.read_csv('State Populations.csv')
+population = {}
+for i, j in population_df.iterrows():
+    population.update({j['State']: int(j['2018 Population'])})
 x = []
 y = []
 state_order = []
+final_population = {}
 for i, j in state_coord.iterrows():
     if j['state'] not in del_states:
         x.append(j['longitude'])
         y.append(j['latitude'])
         state_order.append(j['state'])
+        final_population.update({j['state']: population[j['name']]})
 del states["HI"]
 del states["AK"]
 state_xs = [states[code]["lons"] for code in states]
@@ -32,8 +38,6 @@ for i, j in test_df.iterrows():
     if j['state'] == 'TX' and math.isnan(j['deathIncrease']) == False:
         summ = summ + j['deathIncrease']
 
-#print(summ)
-
 deathsByState = {}
 testsByState = {}
 for i in state_order:
@@ -42,12 +46,10 @@ for i in state_order:
     new_df = new_df[new_df['state'] == str(i)]
     new_df['date'] = pd.to_datetime(new_df['date'], format="%Y/%m/%d")
     new_df = new_df[new_df['date'].dt.month >= 3]
-    new_df = new_df[new_df['date'].dt.month <= 10]
+    new_df = new_df[new_df['date'].dt.month <= 11]
     new_df['X_date'] = pd.to_datetime(new_df['X_date'], format="%Y/%m/%d")
     new_df.set_index('date', inplace=True)
     resampled_df = new_df.resample('M').sum()
-    # print(i)
-    # print(resampled_df)
     death = resampled_df['deathIncrease'].to_list()
     tests = resampled_df['totalTestResultsIncrease'].to_list()
     deathsByState.update({i: death})
@@ -57,26 +59,22 @@ size_list = []
 actual_size = []
 color_list = []
 for i in deathsByState:      # color
-    # print(deathsByState[i][6])
     size_list.append(float(deathsByState[i][6]))
     actual_size.append(deathsByState[i][6])
 
 for i in testsByState:      # size
-    color_list.append(float(testsByState[i][6])/25000)
-
-print(color_list)
-print(size_list)
+    color_list.append((float(testsByState[i][6])/final_population[i])*250)
 
 p = figure(title="COVID-19 Number of Deaths vs Testing: September", plot_width=1200, plot_height=800)
 p.patches(state_xs, state_ys, line_color="#000000", line_width=2, fill_color="#ffffff")
 source = ColumnDataSource(dict(x=x, y=y, size_list=size_list, color_list=color_list, actual_size=actual_size))
 mapper = linear_cmap(field_name='size_list', palette=Spectral11, low=min(size_list), high=max(size_list))
 renderer = p.circle(source=source, x='x', y='y', size='color_list', fill_color=mapper)
-color_bar = ColorBar(title='Number of Deaths', color_mapper=mapper['transform'], width=10)
-relation = {3: 0, 4: 1, 5: 2, 6: 3, 7: 4, 8: 5, 9: 6, 10: 7}
-months = {3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October'}
+color_bar = ColorBar(title='Deaths', color_mapper=mapper['transform'], width=10)
+relation = {3: 0, 4: 1, 5: 2, 6: 3, 7: 4, 8: 5, 9: 6, 10: 7, 11: 8}
+months = {3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November'}
 event_legend1 = Legend(items=[LegendItem(label='500', renderers=[renderer])],
-                       location=(27, 125), label_height=3, label_standoff=10, title="Number of Tests")
+                       location=(27, 125), label_height=3, label_standoff=10, title="Tests per 250")
 event_legend2 = Legend(items=[LegendItem(label='1500', renderers=[renderer])],
                        location=(21, 108), label_standoff=5)
 event_legend3 = Legend(items=[LegendItem(label='3000', renderers=[renderer])],
@@ -105,24 +103,20 @@ def update_plot(attr, old, new):
     global mapper
     global p
     global actual_size
-    global hover_tool
     test = []
     test2 = []
     for k in deathsByState:
-        test.append(float(deathsByState[k][relation[month]]))     # normalized
+        test.append(float(deathsByState[k][relation[month]]))
     for l in testsByState:
-        test2.append(float(testsByState[l][relation[month]])/25000)
+        test2.append((float(testsByState[l][relation[month]])/final_population[l])*250)
     p.title.text = "COVID-19 Number of Deaths vs Testing: " + str(months[month])
     source.data['size_list'] = test
     source.data['color_list'] = test2
-    print(test)
-    print(test2)
-    print(' ')
     color_list = test
     mapper = test
 
 
-slider = Slider(title='Month:', start=3, end=10, step=1, value=9)
+slider = Slider(title='Month:', start=3, end=11, step=1, value=9)
 slider.on_change('value', update_plot)
 layout = column(p, slider)
 p.add_layout(color_bar, 'right')
